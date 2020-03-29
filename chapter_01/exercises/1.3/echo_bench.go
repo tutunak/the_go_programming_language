7d20aabad752e76394a32c93ea7b4b1ef7763dc7
tutunak <dk@tutunak.com>
2020-03-29 15:33:49 +0300

    simple bench

--- chapter_01/exercises/1.3/echo_bench.go
+++ chapter_01/exercises/1.3/echo_bench.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"time"
+	"os"
 	"strings"
+	"time"
 )
 
-
 func main() {
+	s, sep := "", ""
+	start := time.Now()
+	for _, value := range os.Args[1:] {
+		s += sep + value
+		sep = " "
+	}
+	fmt.Println(s)
+	forRange := time.Since(start).Seconds()
 
+	fmt.Printf("For range: %.10fs\n\n", forRange)
+
+	start = time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	join := time.Since(start).Seconds()
+
+	fmt.Printf("Join: %.10fs\n\n", join)
 }
